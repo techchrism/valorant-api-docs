90f1273cd2bea0a742759a7821687d9ca7076416
techchrism <chris@techchrism.me>
2022-08-07 10:41:22 -0700

    remove GitHub url trailing slash

--- generation/lib/DataManager.js
+++ generation/lib/DataManager.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises;
 const path = require('path');
 const Mustache = require('mustache');
 
-const githubURL = 'https://github.com/techchrism/valorant-api-docs/tree/trunk/';
+const githubURL = 'https://github.com/techchrism/valorant-api-docs/tree/trunk';
 
 class DataManager
 {
