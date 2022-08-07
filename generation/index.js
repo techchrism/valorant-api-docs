14d7463cdaf8bb2039d37feee3c4478bba5dbaf5
techchrism <chris@techchrism.me>
2022-08-07 00:26:49 -0700

    use case-sensitive require

--- generation/index.js
+++ generation/index.js
@@ -2,19 +2,19 @@ const fs = require('fs').promises;
 const path = require('path');
 const endpointsToInsomnia = require('./lib/endpointsToInsomnia');
 const generateMarkdownDocs = require('./lib/generateMarkdownDocs');
-const DataManager = require('./lib/dataManager');
+const DataManager = require('./lib/DataManager');
 
 (async () =>
 {
     const dataDir = path.join(__dirname, '..', 'data');
     const docsDir = path.join(__dirname, '..', 'docs');
-    
+
     const dataManager = new DataManager(dataDir);
     await dataManager.readFiles();
-    
+
     // Generate markdown docs
     await generateMarkdownDocs(docsDir, dataManager);
-    
+
     // Form insomnia import
     await fs.writeFile(path.join(docsDir, 'valorant-workspace-insomnia.json'), JSON.stringify(endpointsToInsomnia(dataManager)), 'utf-8');
 })();
