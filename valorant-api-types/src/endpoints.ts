5b08298f9a0465398bb267040cf07eb323c2f89d
techchrism <chris@techchrism.me>
2023-01-30 20:53:16 -0800

    add PlayerLoadout endpoint

--- valorant-api-types/src/endpoints.ts
+++ valorant-api-types/src/endpoints.ts
@@ -1,7 +1,9 @@
 import {fetchContentEndpoint} from './endpoints/pvp/FetchContent'
 import {accountXPEndpoint} from './endpoints/pvp/AccountXP'
+import {playerLoadoutEndpoint} from './endpoints/pvp/PlayerLoadout'
 
 export const endpoints = {
     fetchContentEndpoint,
-    accountXPEndpoint
+    accountXPEndpoint,
+    playerLoadoutEndpoint
 }
--- valorant-api-types/src/index.ts
+++ valorant-api-types/src/index.ts
@@ -1,4 +1,5 @@
 export * from './endpoints/pvp/FetchContent'
 export * from './endpoints/pvp/AccountXP'
+export * from './endpoints/pvp/PlayerLoadout'
 
 export * from './endpoints'
