e5e3ee08f1bc10f7d977cd2e964ac8f78c1a7d92
techchrism <chris@techchrism.me>
2023-02-09 14:27:46 -0800

    feat: update cid option in ChatParticipants endpoint to be optional

--- valorant-api-types/src/endpoints/local/chat/ChatParticipants.ts
+++ valorant-api-types/src/endpoints/local/chat/ChatParticipants.ts
@@ -4,12 +4,12 @@ import {participantsSchema} from '../../../commonTypes'
 
 export const chatParticipantsEndpoint = {
     name: 'Chat Participants',
-    description: 'Get information about the participants of a chat',
+    description: 'Get information about the participants of all active conversations or a specific conversation if a cid is provided',
     category: 'Local Endpoints/Chat',
     type: 'local',
     suffix: 'chat/v5/participants',
     query: new Map([
-        ['cid', z.string()]
+        ['cid', z.string().optional()]
     ]),
     riotRequirements: {
         localAuth: true
