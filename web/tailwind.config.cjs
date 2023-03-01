da6ef8048efc50afda0fc91ce7f9df12ef17a2cf
techchrism <chris@techchrism.me>
2023-03-01 12:55:04 -0800

    feat(web): add basic responsive sidebar

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				'main': '300px 1fr',
+			}
+		},
 	},
 	plugins: [],
 	darkMode: 'media'
