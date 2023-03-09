480c3864244c95ad98eeb80540948e4d148c903c
techchrism <chris@techchrism.me>
2023-03-08 17:14:18 -0800

    fix(web): use astro `file` build option to prevent cloudflare pages from adding trailing slashes

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  build: {
+    format: 'file'
+  }
 });
