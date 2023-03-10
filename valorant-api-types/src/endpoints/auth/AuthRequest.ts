27e21749b6f5f6bd1376f404c9af48f00dfc2b35
techchrism <chris@techchrism.me>
2023-03-10 11:49:06 -0800

    refactor(types): use alternate endpoint linking syntax for cleaner fallback

--- valorant-api-types/src/endpoints/auth/AuthRequest.ts
+++ valorant-api-types/src/endpoints/auth/AuthRequest.ts
@@ -4,7 +4,7 @@ import {z} from 'zod'
 export const authRequestEndpoint = {
     name: 'Auth Request',
     description: 'Perform authorization request to get token  \n' +
-        'Requires cookies from the [Auth Cookies]({{#linkto}}Auth Cookies{{/linkto}}) stage. The token can be found in the `uri` property.',
+        'Requires cookies from the [POST Auth Cookies] stage. The token can be found in the `uri` property.',
     category: 'Authentication Endpoints',
     type: 'other',
     suffix: 'https://auth.riotgames.com/api/v1/authorization',
--- valorant-api-types/src/endpoints/auth/CookieReauth.ts
+++ valorant-api-types/src/endpoints/auth/CookieReauth.ts
@@ -3,7 +3,7 @@ import {ValorantEndpoint} from '../../ValorantEndpoint'
 export const cookieReauthEndpoint = {
     name: 'Cookie Reauth',
     description: 'Get a new token using the cookies from a previous authorization request\n' +
-        'Use the saved cookies from [Auth Request]({{#linkto}}Auth Request{{/linkto}}) (specifically the `ssid` cookie). The token can be found from the url this request redirects to.\n' +
+        'Use the saved cookies from [PUT Auth Request] (specifically the `ssid` cookie). The token can be found from the url this request redirects to.\n' +
         'Recommended to use this endpoint instead of storing the password and sending it again.',
     category: 'Authentication Endpoints',
     type: 'other',
