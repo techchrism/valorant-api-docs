97aaf5549e8d0038fd583ac07f362da407c0b87c
Techdoodle <chris@techchrism.me>
2023-03-15 12:25:56 -0700

    update name service descriptions

--- valorant-api-types/src/endpoints/pvp/NameService.ts
+++ valorant-api-types/src/endpoints/pvp/NameService.ts
@@ -4,7 +4,7 @@ import {playerUUIDSchema} from '../../commonTypes'
 
 export const nameServiceEndpoint = {
     name: 'Name Service',
-    description: 'Get a players name and tagline by the PlayerUUID',
+    description: 'Get a player's name and tagline by their PUUID. Supports retrieving multiple players in one request.',
     queryName: 'NameService_GetPlayerInfo',
     category: 'PVP Endpoints',
     type: 'pd',
@@ -18,7 +18,7 @@ export const nameServiceEndpoint = {
             TagLine: z.string(),
         }))
     },
-    body: z.array(playerUUIDSchema).describe("An array of playerUUIDs you want to get the NameService for")
+    body: z.array(playerUUIDSchema).describe("PUUID(s) to retrieve the name data for")
 } as const satisfies ValorantEndpoint
 
 export type NameServiceResponse = z.input<typeof nameServiceEndpoint.responses['200']>
