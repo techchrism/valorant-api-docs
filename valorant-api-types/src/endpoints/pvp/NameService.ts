c0bf413019f17afee194a5477e12ea21a175cdc1
tim <tim@zolleis.net>
2023-03-15 20:15:15 +0100

    add name-service endpoint

---
+++ valorant-api-types/src/endpoints/pvp/NameService.ts
@@ -0,0 +1,24 @@
+import {ValorantEndpoint} from '../../ValorantEndpoint'
+import {z} from 'zod'
+import {playerUUIDSchema} from '../../commonTypes'
+
+export const nameServiceEndpoint = {
+    name: 'Name Service',
+    description: 'Get a players name and tagline by the PlayerUUID',
+    queryName: 'NameService_GetPlayerInfo',
+    category: 'PVP Endpoints',
+    type: 'pd',
+    method: 'PUT',
+    suffix: 'name-service/v2/players',
+    responses: {
+        '200': z.array(z.object({
+            DisplayName: z.string(),
+            Subject: playerUUIDSchema,
+            GameName: z.string(),
+            TagLine: z.string(),
+        }))
+    },
+    body: z.array(z.string()).describe("An array of playerUUIDs you want to get the NameService for")
+} as const satisfies ValorantEndpoint
+
+export type NameServiceResponse = z.input<typeof nameServiceEndpoint.responses['200']>
