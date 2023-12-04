9ddd50ba178fda5b37e7cc00afd1bc9d5466586e
techchrism <chris@techchrism.me>
2023-12-03 16:24:37 -0800

    feat(web): link to new workspace generation and use Insomnia url schemes

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://valapidocs.techchrism.me',
     integrations: [tailwind()],
     build: {
         format: 'file'
